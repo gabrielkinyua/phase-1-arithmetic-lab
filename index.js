31018bc5f4ee311c86982cd99c04a076d807f27a
gabriel <gabzkinyua@gmail.com>
2024-11-28 23:57:57 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 16;
+let num4 = 6;
+let multiply = num1 * num2;
+let random = Math.round(Math.random() * 5) + 1;
+let mod = num3 % num4;
+let max = Math.max(8, 15, 20);
